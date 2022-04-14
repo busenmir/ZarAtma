ec622b1d39e0eb1d4a2e236330cdc9c5185fb283
Buse Demir <buse93dmr@gmail.com>
2022-04-14 14:57:54 +0300

    Zar Atmak

---
+++ app/src/main/java/com/busedemir/upschool1/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.busedemir.upschool1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+public var rastgeleSayi = 0
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun button3(view: View) {
+        rastgeleSayi = Random.nextInt(7)
+        imageView.setImageResource(when(rastgeleSayi){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        })
+    }
+}
